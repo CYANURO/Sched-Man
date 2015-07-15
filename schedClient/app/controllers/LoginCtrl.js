89f5e2bae2f681a3979679d7003c6bb6ce0bcb11
Miguel Lopez <mlopez4@mmm.com>
2015-07-15 11:54:08 -0600

    Added skeleton controllers for login and user dashboard.
    Added some documentation (partial requirements capture: use cases)

--- schedClient/app/controllers/LoginCtrl.js
+++ schedClient/app/controllers/LoginCtrl.js
@@ -8,7 +8,22 @@
                     [ctrl]);
 
     function ctrl() {
-        var vm = this;
+        
+        var vm = this,
+            _password = 123;
+
+
+
+        vm.userName = '';
+        vm.password = '';
+
+        vm.signIn = function(){
+
+            // login service
+            
+
+        };
+
     }
 
 }());
